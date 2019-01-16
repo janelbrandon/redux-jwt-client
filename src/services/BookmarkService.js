f1f1fc8761ec53204338dc9806b6b52870daa0bd
caadmin <janel.brandon@coderacademy.edu.au>
2019-01-17 09:29:50 +1000

    remove createBookmark from this branch

--- src/services/BookmarkService.js
+++ src/services/BookmarkService.js
@@ -26,29 +26,4 @@ const removeBookmark = (id) => {
 
 }
 
-const createBookmark = (event) => {
-    event.preventDefault()
-    const form = event.target
-    console.log(form)
-    const title = form.elements.title.value
-    const url = form.elements.url.value
-    if (title.length === 0 || url.length === 0) {
-        alert('Bookmark title and url are required')
-        return
-    }
-    api.post('/bookmarks', {
-        title: form.elements.title.value,
-        url: form.elements.url.value
-    }).then((res) => {
-        const newBookmarks = [...store.getState().bookmarks, res.data]
-        store.dispatch(setBookmarksAction(newBookmarks))
-        fetchBookmarks()
-        form.elements.title.value = ''
-        form.elements.url.value = ''
-    }).catch((error) => {
-        console.error('Error creating bookmark: ', error)
-    })
-}
-
-
-export { fetchBookmarks, createBookmark, removeBookmark } 
+export { fetchBookmarks, removeBookmark } 
